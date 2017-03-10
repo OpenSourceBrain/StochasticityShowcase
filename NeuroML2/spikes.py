2d18118716fd871f2115e6a233601cbad7ab1843
Padraig Gleeson <padraig@Padraigs-MacBook-Pro.local>
2017-03-10 10:09:00 +0000

    More checks on spiking syn inputs

--- NeuroML2/spikes.py
+++ NeuroML2/spikes.py
@@ -1,10 +1,11 @@
 
 import numpy as np
 
-files = ['regular.spikes','random.spikes','poisson.spikes','pynn.spikes']
+files = ['regular.spikes','random.spikes','poisson.spikes','pynn.spikes','pois.syn.fire','trans.pois.syn.fire']
 
-tstop = 10000.0
+tstop = 20000.0
 expected_avg_rate = 50
+expected_std_isi = 20
 
 for fn in files:
     f = open(fn)
@@ -23,5 +24,9 @@ for fn in files:
     print('\n--- Analysing: %s'%fn)
     
     avg_rate = 1000/(tstop/len(spikes))
-    print("Num spikes: %s; avg rate: %s Hz; avg isi: %s ms; std isi: %s ms"%(len(spikes),avg_rate,np.average(isis),np.std(isis)))
-    assert abs(avg_rate-expected_avg_rate) < 1
+    std_isi = np.std(isis)
+    print("Num spikes: %s; avg rate: %s Hz; avg isi: %s ms; std isi: %s ms"%(len(spikes),avg_rate,np.average(isis),std_isi))
+    assert abs(avg_rate-expected_avg_rate) <= 1
+    if 'pois' in fn or 'pynn' in fn:
+        assert abs(std_isi-expected_std_isi) <= 1
+        
